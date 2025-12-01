e2786e4638385ed494e00af842c658c5706f8d12
Axtwal <ademarteiyuchina@gmail.com>
2025-12-01 20:43:19 -0300

    fix: corrigindo erro das rotas

--- Gengo-front/src/app/app.routes.ts
+++ Gengo-front/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { Home } from './components/home/home';
+import { TelaLoginRegister } from './components/tela-login-register/tela-login-register';
 
 export const routes: Routes = [
-    {path:"",component:Home}
+    {path:"",component:TelaLoginRegister}
 ];
